ecf78bad2f26eabb7d2ac3a45e152ff05739c0ad
hitode909 <hitode909@gmail.com>
2016-11-15 19:19:05 +0900

    init

---
+++ spec/atom-auto-programming-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomAutoProgrammingView from '../lib/atom-auto-programming-view';
+
+describe('AtomAutoProgrammingView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
